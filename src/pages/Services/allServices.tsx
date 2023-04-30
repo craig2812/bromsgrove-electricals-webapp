644217968d77e32ea1f4c4c4465e9b5dbef7b160
craig2812 <craig.farrell@blackbaud.co.uk>
2023-04-30 13:33:55 +0100

    remove uneccessary imports

--- src/pages/Services/allServices.tsx
+++ src/pages/Services/allServices.tsx
@@ -1,9 +1,5 @@
 import * as React from 'react';
 import './services.css';
-import Charging from './charging'
-import Commercial from './commerical';
-import Domestic from './domestic';
-import Testing from './testing';
 import pageData from "../../pages.json";
 import { ServicesPage } from '../../components/ServicesPage/servicesPage';
 import {backdropItem} from '../../components/Backdrop/backdrop'
