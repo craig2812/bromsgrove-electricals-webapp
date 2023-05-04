d298b46167e1cf6f55f671792de9e7641c9abc9d
craig2812 <craig.farrell@blackbaud.co.uk>
2023-05-04 17:02:27 +0100

    adding first firebase controls for picture upload

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -1,25 +1,29 @@
 import * as React from 'react';
-import { useEffect, useState } from 'react';
+import { addPicture, fetchPics, getPictures } from '../../firebase/getData';
 import '../page.css';
+import { db } from '../../firebase/getData';
 
 const Home = () => {
 
-// const [backEndData, setBackEndData] = useState([{}])
-
-// useEffect(()=>{
-//   //this is calling package.json proxy value plus adding /content to end of path
-//   fetch("/content").then(
-//     response => response.json()).then(
-//       data => {
-//         setBackEndData(data)}
-//     )
-//       }, [])
-// console.log('data', backEndData)
-
+const pictures = getPictures(db)
+const pics = fetchPics()
+function addPic(){
+addPicture({
+    descritption: "Testing",
+    id: 12,
+    url: '',
+    type: '',
+    isShown: false
+})}
+pics.then(pic => pic.forEach(picture => {
+    console.log(picture)
+}))
     return (
         <div className="page-container">
             <h1>Welcome to Bromsgrove Electricals</h1>
             Bromsgrove Electricals provides reliable and professional electrical services for your home or business. Our services include indoor and outdoor installations, routine checks, and vehicle charge point installation. Trust our experienced electricians to deliver exceptional results and ensure your satisfaction.
+            
+            <button onClick={addPic}>Add Picture</button>
             </div>
     );
 };
